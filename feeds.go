43a98a4f89da12e5e2c914472baa7368fd8c7f26
Minetest-j45 <55553015+Minetest-j45@users.noreply.github.com>
2022-03-06 11:41:25 +0000

    add feeds and file to generate them (will use gh actions)

---
+++ feeds.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"io/ioutil"
+	"strings"
+	"time"
+
+	"github.com/gorilla/feeds"
+)
+
+type Blog struct {
+	Title string
+	Date  string
+	Text  string
+}
+
+func main() {
+	feed := &feeds.Feed{
+		Title:       "j45's Blog",
+		Link:        &feeds.Link{Href: "https://j1233.minetest.land/blog/"},
+		Description: "j45's Blog",
+		Author:      &feeds.Author{Name: "j45"},
+		Created:     time.Now(),
+	}
+
+	var blogs []Blog
+
+	var feedItems []*feeds.Item
+
+	dat, err := ioutil.ReadFile("blogs.csv")
+	if err != nil {
+		panic(err)
+	}
+	lines := strings.Split(string(dat), "\n")
+
+	lines = lines[1 : len(lines)-1]
+	for _, line := range lines {
+		fields := strings.Split(line, "|")
+		blog := Blog{
+			Title: fields[0],
+			Text:  fields[2],
+		}
+		blogs = append(blogs, blog)
+	}
+
+	for _, blog := range blogs {
+		titleslice := strings.Split(blog.Title, " ")
+		link := "https://j1233.minetest.land/blog/#" + strings.Join(titleslice, "_")
+		feedItems = append(feedItems,
+			&feeds.Item{
+				Title:       blog.Title,
+				Link:        &feeds.Link{Href: link},
+				Description: blog.Text,
+			})
+	}
+	feed.Items = feedItems
+
+	rssFeed, err := feed.ToRss()
+	if err != nil {
+		panic(err)
+	}
+
+	err = ioutil.WriteFile("feed.rss", []byte(rssFeed), 0644)
+	if err != nil {
+		panic(err)
+	}
+
+	jsonFeed, err := feed.ToJSON()
+	if err != nil {
+		panic(err)
+	}
+
+	err = ioutil.WriteFile("feed.json", []byte(jsonFeed), 0644)
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/Minetest-j45/blog
+
+go 1.17
+
+require github.com/gorilla/feeds v1.1.1
+
+require github.com/kr/pretty v0.3.0 // indirect
